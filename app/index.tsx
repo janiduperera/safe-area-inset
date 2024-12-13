e9c6b7ebb422438c6d31881d9036caf3c4306026
janiduperera <pereraprabash@gmail.com>
2024-12-13 12:46:07 +0100

    bug fixes

--- app/index.tsx
+++ app/index.tsx
@@ -11,8 +11,8 @@ import {
 import { useRef, useState } from "react";
 
 type safeAreaInset = {
-  top: number,
-  left: number,
+  top: number;
+  left: number;
 };
 
 type dataArray = string[];
@@ -20,27 +20,40 @@ type dataArray = string[];
 type popUpPositions = {
   left: number;
   top: number;
-};;
+};
 
 export default function Index() {
-
   const chevronDownBlack = require("@/assets/images/chevron-down-black.png");
   const chevronUpBlack = require("@/assets/images/chevron-up-black.png");
 
   const dropDownRef = useRef<View | null>(null);
   const getInsetPosRef = useRef<View | null>(null);
+
+  // For some reason, if the SafeAreaView does not give the proper insets to calculate the position of a certain element such as button
+  // This values will help
   const [safeAreaInsets, setSafeAreaInsets] = useState<safeAreaInset>({
     top: 0,
-    left: 0
-  })
+    left: 0,
+  });
   const [popUpPositions, setPopUpPositions] = useState<popUpPositions>({
     left: 0,
     top: 0,
   });
 
-  const [modalVisible, setModalVisible] = useState<boolean>(false);
   const [clicked, setClicked] = useState<boolean>(false);
-  const [data, setData] = useState<dataArray>(['Item 1', 'Item 2', 'Item 3', 'Item 4', 'Item 5', 'Item 6', 'Item 7', 'Item 8', 'Item 9', 'Item 10', 'Item 11']);
+  const [data, setData] = useState<dataArray>([
+    "Item 1",
+    "Item 2",
+    "Item 3",
+    "Item 4",
+    "Item 5",
+    "Item 6",
+    "Item 7",
+    "Item 8",
+    "Item 9",
+    "Item 10",
+    "Item 11",
+  ]);
   const [selectedItem, setSelectedItem] = useState<string>("");
 
   const onDropDownClicked = (): void => {
@@ -85,57 +98,57 @@ export default function Index() {
     </View>
   );
 
-
   return (
-    <SafeAreaView
-      style={styles.container}
-    >
+    <SafeAreaView style={styles.container}>
       {/* The whole purpose of this view is to calculae the safe area inset top and left.  */}
       <View
-          ref={getInsetPosRef}
-          style={{
-            position: 'absolute',
-            width: 0.1,
-            height: 0.1,
-            left: 0,
-            top: 0,
-            zIndex: -100,
-          }}
-          onLayout={(evt) => {
-            getInsetPosRef?.current?.measure(
-              ( pagex, pagey) => {
-                setSafeAreaInsets(
-                  {
-                    top: pagey,
-                    left: pagex
-                  }
-                )
-              }
-            );
-          }}
-        />
+        ref={getInsetPosRef}
+        style={{
+          position: "absolute",
+          width: 0.1,
+          height: 0.1,
+          left: 0,
+          top: 0,
+          zIndex: -100,
+        }}
+        onLayout={(evt) => {
+          getInsetPosRef?.current?.measure((pagex, pagey) => {
+            setSafeAreaInsets({
+              top: pagey,
+              left: pagex,
+            });
+          });
+        }}
+      />
       <View style={styles.dropDownSection}>
-      <Pressable onPress={() => onDropDownClicked()}>
-        <View ref={dropDownRef} style={styles.dropDownButton}>
-          <Text style={styles.dropDownButton_Text}>
-            {selectedItem === "" ? "Select" : selectedItem}
-          </Text>
-          <View style={styles.dropDownButton_ArrowBox}>
-            <Image
-              style={styles.dropDownButton_ArrowBox_Chevron}
-              source={clicked ? chevronUpBlack : chevronDownBlack}
-            ></Image>
+        <Pressable onPress={() => onDropDownClicked()}>
+          <View ref={dropDownRef} style={styles.dropDownButton}>
+            <Text style={styles.dropDownButton_Text}>
+              {selectedItem === "" ? "Select" : selectedItem}
+            </Text>
+            <View style={styles.dropDownButton_ArrowBox}>
+              <Image
+                style={styles.dropDownButton_ArrowBox_Chevron}
+                source={clicked ? chevronUpBlack : chevronDownBlack}
+              ></Image>
+            </View>
           </View>
-        </View>
-      </Pressable>
-    </View>
-    <Modal
+        </Pressable>
+      </View>
+      <Modal
         animationType="fade"
         transparent={true}
         visible={clicked}
         onRequestClose={() => {
           setClicked(!clicked);
         }}
+        supportedOrientations={[
+          "portrait",
+          "portrait-upside-down",
+          "landscape",
+          "landscape-left",
+          "landscape-right",
+        ]}
       >
         {dropDownMenu}
       </Modal>
@@ -148,7 +161,7 @@ const styles = StyleSheet.create({
     flex: 1,
     justifyContent: "flex-start",
     alignItems: "center",
-    padding: 50,
+    backgroundColor: "red",
   },
 
   dropDownSection: {
